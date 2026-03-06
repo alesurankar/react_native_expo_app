7c21bb340dc2b86f192c2ccdec10aa3fae72550a
ales2026 <ale.urankar@gmail.com>
2026-03-06 17:47:49 +0100

    extract AddNoteModal

--- app/notes/index.tsx
+++ app/notes/index.tsx
@@ -4,10 +4,9 @@ import {
     Text, 
     StyleSheet, 
     TouchableOpacity,
-    Modal,
-    TextInput
 } from "react-native";
 import NoteList from '../../components/NoteList';
+import AddNoteModal from '../../components/AddNoteModal';
 
 const NoteScreen = () => {
     const [notes, setNotes] = useState([
@@ -41,35 +40,14 @@ const NoteScreen = () => {
             </TouchableOpacity>
 
             {/* Modal */}
-            <Modal
-                visible={modalVisible}
-                animationType='slide'
-                transparent
-                onRequestClose={() => setModalVisible(false)}
-            >
-                <View style={styles.modalOverlay}>
-                    <View style={styles.modalContent}>
-                        <Text style={styles.modalTitle}>Add a New Note</Text>
-                        <TextInput 
-                            style={styles.input}
-                            placeholder='Enter note...'
-                            placeholderTextColor='#aaa'
-                            value={newNote}
-                            onChangeText={setNewNote}
-                        />
-                        <View style={styles.modalButtons}>
-                            <TouchableOpacity style={styles.cancelButton} onPress={ () => setModalVisible(false) }>
-                                <Text style={styles.cancelButtonText}>Cancel</Text>
-                            </TouchableOpacity>
+            <AddNoteModal 
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                newNote={newNote}
+                setNewNote={setNewNote}
+                addNote={addNote}
+            />
 
-                            <TouchableOpacity style={styles.saveButton} onPress={ addNote }>
-                                <Text style={styles.saveButtonText}>Save</Text>
-                            </TouchableOpacity>
-
-                        </View>
-                    </View>
-                </View>
-            </Modal>
         </View> 
     );
 };
@@ -94,59 +72,6 @@ const styles = StyleSheet.create({
         color: '#ffffff',
         fontSize: 18,
         fontWeight: 'bold',
-    },
-    modalOverlay: {
-        flex: 1,
-        backgroundColor: '#rgba(0,0,0,0.5)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    modalContent: {
-        backgroundColor: '#ffffff',
-        padding: 20,
-        borderRadius: 10,
-        width: '80%',
-    },
-    modalTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-        textAlign: 'center',
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 8,
-        padding: 10,
-        fontSize: 16,
-        marginBottom: 15,
-    },
-    modalButtons: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    cancelButton: {
-        backgroundColor: '#ccc',
-        padding: 10,
-        borderRadius: 5,
-        flex:1, 
-        marginRight: 10,
-        alignItems: 'center',
-    },
-    cancelButtonText: {
-        fontSize: 16,
-        color: '#333',
-    },
-    saveButton: {
-        backgroundColor: '#007bff',
-        padding: 10,
-        borderRadius: 5,
-        flex: 1,
-        alignItems: 'center',
-    },
-    saveButtonText: {
-        fontSize: 16,
-        color: '#ffffff'
     },
 });
 
